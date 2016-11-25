266800fc8db29b8662d6fcdd0a85c275a6f2ef78
FlorianPO <florian.popek@gmail.com>
2016-11-25 08:59:09 +0100

    Label position working in RadialMenu

--- MarkingMenu/radialmenu.cpp
+++ MarkingMenu/radialmenu.cpp
@@ -10,30 +10,41 @@ void RadialMenu::setLabelList(const std::vector<std::string>& string_vector) {
 }
 
 void RadialMenu::addLabel(std::string string) {
-   QLabel* label = new QLabel(this);
-   label->setText(QString::fromStdString(string));
-   label->setAutoFillBackground(false);
-   label->setStyleSheet(QLatin1String("font: 75 10pt \"Calibri\";\n" "font-weight: bold;\n"));
-   label->setAlignment(Qt::AlignCenter);
-   label->setFrameShape(QFrame::StyledPanel);
-   label->setFrameShadow(QFrame::Sunken);
+    if (label_vector.size() >= 8)
+        throw std::string("ERREUR : maximul number of labels reached !");
 
-   label->resize(QSize(string.size()*10, 25));
-   label->show();
+    QLabel* label = new QLabel(this);
+    label->setText(QString::fromStdString(string));
+    label->setAutoFillBackground(false);
+    label->setStyleSheet(QLatin1String("font: 75 10pt \"Calibri\";\n" "font-weight: bold;\n"));
+    label->setAlignment(Qt::AlignCenter);
+    label->setFrameShape(QFrame::StyledPanel);
+    label->setFrameShadow(QFrame::Sunken);
 
-   label_vector.push_back(label);
+    label->resize(QSize(string.size()*10, 25));
+    label->show();
+
+    label_vector.push_back(label);
 }
 
 void RadialMenu::showMenu(const QPoint& position) {
     center = position;
 
-    float angle = 360.f / label_vector.size();
+    float angle = 0;
+    switch (label_vector.size()) {
+        case 1: break;
+        case 2: angle = 180.f; break;
+        default:
+            angle = 90.f;
+    }
 
     QPoint point_orig(center.x() + distance, center.y());
     for (int i=0; i < label_vector.size(); i++) {
         label_vector[i]->move(point_orig.x() - label_vector[i]->size().width()/2,
                               point_orig.y() - label_vector[i]->size().height()/2);
-        point_orig = rotationGlobal(point_orig, center, angle);
+        if (i%4 == 3 && i > 0)
+            point_orig = rotationGlobal(point_orig, center, -45);
+        point_orig = rotationGlobal(point_orig, center, -angle);
     }
 
     this->show();
